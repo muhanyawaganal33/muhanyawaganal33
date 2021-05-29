98f82205df12e1279529ca1aed445ca759020250
muhanyawaganal33 <85038601+muhanyawaganal33@users.noreply.github.com>
2021-05-30 05:01:42 +0800

    Update muhanyawaganal.sh

--- muhanyawaganal.sh
+++ muhanyawaganal.sh
@@ -1,4 +1,5 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
 tar xf SRBMiner-Multi-0-7-6-Linux.tar.xz
 cd SRBMiner-Multi-0-7-6
+chmod +x SRBMiner-MULTI
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3957 --wallet RFj4WWD72iGHn2Q6swr11BzKXhnYpAEzGr 
